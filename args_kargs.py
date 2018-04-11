9103a04dfb9b2bc709d31c0d2f56c83f3f68e2d2
Konda Chendil Munireddy <mchendil@MacBook-Pro-4.local>
2018-04-10 20:33:52 -0500

    updated comments in args and kargs python file

--- args_kargs.py
+++ args_kargs.py
@@ -1,8 +1,12 @@
-def kkargs(fname,lname,*args, **kwargs):
+def kargs_args(*args, **kwargs):
     print(args)
     print(kwargs)
 
 fname="konda chendil"
 lname="munireddy"
 
-kkargs(fname="1",lname="2")
+
+kargs_args(fname="1",lname="2")
+kargs_args("1","2")
+
+#if we pass args and kargs in same function, technically either one is intialized based on data passed... if passed data is keyword argument then it kargs will intialzed else it is series of values args list is intialized
