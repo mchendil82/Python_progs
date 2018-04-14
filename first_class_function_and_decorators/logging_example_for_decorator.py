d6880ebe371839ef5f3d51c7b2ac7c2c591cdf46
Konda Chendil Munireddy <mchendil@MacBook-Pro-4.local>
2018-04-14 11:13:25 -0500

    example of decorator with logging use case

--- first_class_function_and_decorators/logging_example_for_decorator.py
+++ first_class_function_and_decorators/logging_example_for_decorator.py
@@ -1,3 +1,6 @@
+#This is use case of decorator to track how many function is called using decorator
+#We call display function and decorator my_logger function calls and record how many time it is called by creating
+#in the name of calling function... here it will create logs under "logs/display_info.log"
 import os
 def my_logger(orig_func):
 	os.mkdir('logs')
