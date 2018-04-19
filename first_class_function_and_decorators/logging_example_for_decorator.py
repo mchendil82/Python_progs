627fed1e2c159920fd04061a126e050655f927cb
Konda Chendil Munireddy <mchendil@MacBook-Pro-4.local>
2018-04-18 20:30:35 -0500

    decorator function stacked and issues realted to it

--- first_class_function_and_decorators/logging_example_for_decorator.py
+++ first_class_function_and_decorators/logging_example_for_decorator.py
@@ -1,55 +1,55 @@
 #This is use case of decorator to track how many function is called using decorator
-#We call display function and decorator my_logger function calls and record how many time it is called by creating
+#We call display function and decorator first_logger_func_decorator function calls and record how many time it is called by creating
 #in the name of calling function... here it will create logs under "logs/display_info.log"
 
 
 
 import os
-def my_logger(orig_func):
+def first_logger_func_decorator(orig_func):
 	if not os.path.exists('logs'):
 		os.mkdir('logs')
 	import logging
 	logging.basicConfig(filename='logs/{}.log'.format(orig_func.__name__), level=logging.DEBUG)
 
 
-	def wrapper_logger(*args, **kwargs):
+	def first_func_wrapper(*args, **kwargs):
 		logging.debug(
 			'Ran with args: {}, and kwargs: {}'.format(args, kwargs))
 		return orig_func(*args, **kwargs)
-	return wrapper_logger
+	return first_func_wrapper
 
 import time
-def my_timer(orig_func):
-	def wrapper_timer(*args, **kwargs):
+def second_timer_func_decorator(orig_func):
+	def second_func_wrapper(*args, **kwargs):
 		t1=time.time()
 		result=orig_func(*args, **kwargs)
 		t2= time.time() - t1
 		print('{} ran in: {} sec'.format(orig_func.__name__, t2))
 		return result
-	return wrapper_timer
+	return second_func_wrapper
 
 
-#Here we have 2 decorators "my_logger" and "mytimer".. 
+#Here we have 2 decorators "first_logger_func_decorator" and "second_timer_func_decorator".. 
 #and we are going to talk about a problem with stacking multiple decorators.. 
 
-#The decorator which in lowest level (here it is @my_timer) is executed first and then next in queue.. 
-#Here my_timer will be executed first and that function retunred or reuslts will be
-#passed to next decorators which will give different results(to @my_logger)... 
+#The decorator which in lowest level (here it is @second_timer_func_decorator) is executed first and then next in queue.. 
+#Here second_timer_func_decorator will be executed first and that function retunred or reuslts will be
+#passed to next decorators which will give different results(to @first_logger_func_decorator)... 
 #remember a decorator just copies the immediate below function code to decorator function
 
-#expectation is here is my_logger and my_timer is to execute the immediate below (i.e. display_info function) produce results
-#my_logger should create "display_info.log" (see 3rd line) but instead it will be "wrapper_timer.log" as my_timer decorator returns "wrapper_timer" as function name
-#my_logger which creates the wrapper_timer.log... 
+#expectation is here is first_logger_func_decorator and second_timer_func_decorator is to execute the immediate below (i.e. display_info function) produce results
+#first_logger_func_decorator should create "display_info.log" (see 3rd line) but instead it will be "second_func_wrapper.log" as second_timer_func_decorator decorator returns "second_func_wrapper" as function name
+#first_logger_func_decorator which creates the second_func_wrapper.log... 
 
-#if I (reverse it) stack "my_timer" first and "my_logger", display_info.log will be created but "wrapper_logger" function will be returned to timer decorator
-#hence the print satement at 27 line will print wrapper_logger function "as ran" and returns and executes the wrapper_logger instead of wrapper_timer...
+#if I (reverse it) stack "second_timer_func_decorator" first and "first_logger_func_decorator", display_info.log will be created but "first_func_wrapper" function will be returned to timer decorator
+#hence the print satement at 27 line will print first_func_wrapper function "as ran in the print staement" and returns and executes the first_func_wrapper instead of second_func_wrapper...
 
 #comment either of decorator and see the difference... and enable both decorators and see the difference....
 
 
 
-@my_logger
-@my_timer
+@first_logger_func_decorator
+@second_timer_func_decorator
 def display_info(name, age):
 	time.sleep(1)
 	print('display function ran with arguments: '+name+", "+str(age))
