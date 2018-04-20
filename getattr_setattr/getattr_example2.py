8b0602838bd1c032268aefbe78d765570066c4a5
Konda Chendil Munireddy <mchendil@MacBook-Pro-4.local>
2018-04-19 23:18:21 -0500

    updated getattr functions with proper comments to understand difference between __getattr__ and property function

--- getattr_setattr/getattr_example2.py
+++ getattr_setattr/getattr_example2.py
@@ -4,20 +4,28 @@ class emp:
 		self.lastname=last
 	def __getattr__(self, *args):
 		self.email=self.firstname+self.lastname+"@gmail.com"
-		return self.email
+		return "returned via __getattr__ function: "+self.email
 	@property
-	def yyyy(self):
+	def print_yahoo_id(self):
 		self.email=self.firstname+self.lastname+"@yahoo.com"
-		return "hi "+self.email
+		return "returned via property geattr function: "+self.email
 	@property
-	def xxxx(self):
+	def print_rediff_id(self):
 		self.email=self.firstname+self.lastname+"@rediff.com"
-		return self.email
+		return "returned via property geattr function: " + self.email
 
 e=emp('konda','chendil')
-print(e.firstname)
-print(e.lastname)
+#get instance variables
+print("Instance variables: " + e.firstname)
+print("Instance variables: " + e.lastname)
+
+#get by default store the variable with values defined in  __getattr__ function and 
+#it will not "Attribute not defined error".. 
+#This can be used to store values by the user(kind of dynammically ) which is NOT defined in the program
+#Here "email1" and "random_variable" are not defined hence __getattr__ function invovked and store the email value inside in it 
 print(e.email1)
-print(e.email2)
-print(e.yyyy)
-print(e.xxxx)
+print(e.random_variable)
+
+#calls gettr function via property methods.....
+print(e.print_yahoo_id)
+print(e.print_rediff_id)
