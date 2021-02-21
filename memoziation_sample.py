294bac8646af0d8b8ce51611a5200da74626ba36
konda chendil munireddy <m.t.chendil@gmail.com>
2021-02-21 16:01:50 -0600

    memoziatio and __call__ method examples

---
+++ memoziation_sample.py
@@ -0,0 +1,58 @@
+'''calculate factorial for a given number using memoziation principle and provide the result. This is mainly to avoid reptative loops and to resue existing results as factorial depdends in previous series result .
+Also memoziation helps to execution faster for large number special for this scenario'''
+
+class Factorial(object):    
+    def __init__(self):
+        self.cache={}       
+    def __call__(self, n):        
+             if n not in (self.cache):
+                  if n == 0:
+                      self.cache[n]=1                      
+                  else:
+                      self.cache[n]=n*self.__call__(n-1)                       
+             return(self.cache[n])
+
+calfac=Factorial()
+for n in range(0, 5):
+    print("{}! = {}". format(n, calfac(n)))
+
+'''Also __call__ method is useful specially to implement API's.. example:  https://stackoverflow.com/questions/5824881/python-call-special-method-practical-example 
+
+I find it useful because it allows me to create APIs that are easy to use (you have some callable object that requires some specific arguments), and are easy to implement because you can use Object Oriented practices.
+
+The following is code I wrote yesterday that makes a version of the hashlib.foo methods that hash entire files rather than strings:
+
+# filehash.py
+import hashlib
+
+
+class Hasher(object):
+    """
+    A wrapper around the hashlib hash algorithms that allows an entire file to
+    be hashed in a chunked manner.
+    """
+    def __init__(self, algorithm):
+        self.algorithm = algorithm
+
+    def __call__(self, file):
+        hash = self.algorithm()
+        with open(file, 'rb') as f:
+            for chunk in iter(lambda: f.read(4096), ''):
+                hash.update(chunk)
+        return hash.hexdigest()
+
+
+md5    = Hasher(hashlib.md5)
+sha1   = Hasher(hashlib.sha1)
+sha224 = Hasher(hashlib.sha224)
+sha256 = Hasher(hashlib.sha256)
+sha384 = Hasher(hashlib.sha384)
+sha512 = Hasher(hashlib.sha512)
+
+
+
+
+
+
+
+'''
